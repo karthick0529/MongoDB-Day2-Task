039f8c085bde52b21104f90b72628cc268a629dd
Karthick R <53260064+karthick0529@users.noreply.github.com>
2024-07-11 11:48:32 +0530

    Create Topics.js

---
+++ databases/Topics.js
@@ -0,0 +1,52 @@
+// Creating & selecting Database
+
+use Guvi;
+
+// Creating collections for Guvi
+
+db.createCollection("topics");
+
+// Inserting values for topics
+
+db.topics.insertMany([
+    {
+        topicId: 1,
+        topic: "RWD",
+        topicDate: new Date("10-oct-2020"),
+    },
+    {
+        topicId: 2,
+        topic: "Bootstrap",
+        topicDate: new Date("11-oct-2020"),
+    },
+    {
+        topicId: 3,
+        topic: "JS",
+        topicDate: new Date("17-oct-2020"),
+    },
+    {
+        topicId: 4,
+        topic: "Async Programming",
+        topicDate: new Date("18-oct-2020"),
+    },
+    {
+        topicId: 5,
+        topic: "React",
+        topicDate: new Date("24-oct-2020"),
+    },
+    {
+        topicId: 6,
+        topic: "React-Redux",
+        topicDate: new Date("25-oct-2020"),
+    },
+    {
+        topicId: 7,
+        topic: "MySQL",
+        topicDate: new Date("31-oct-2020"),
+    },
+    {
+        topicId: 8,
+        topic: "MongoDB",
+        topicDate: new Date("01-nov-2020"),
+    },
+]);
