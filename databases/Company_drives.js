e2d9d86173f38288455d81e0487b8fc1582e1455
Karthick R <53260064+karthick0529@users.noreply.github.com>
2024-07-11 12:02:25 +0530

    Update Company_drives.js

--- databases/Company_drives.js
+++ databases/Company_drives.js
@@ -8,27 +8,27 @@ Guvi> db.createCollection("companyDrives");
 Guvi> db.companyDrives.insertMany([
 ...     {
 ...         userId: 1,
-...         driveDate: new Date("04-oct-2024"),
+...         driveDate: new Date("04-oct-2020"),
 ...         company: "Google",
 ...     },
 ...     {
 ...         userId: 1,
-...         driveDate: new Date("11-oct-2024"),
+...         driveDate: new Date("11-oct-2020"),
 ...         company: "Amazon",
 ...     },
 ...     {
 ...         userId: 2,
-...         driveDate: new Date("20-oct-2024"),
+...         driveDate: new Date("20-oct-2020"),
 ...         company: "Guvi",
 ...     },
 ...     {
 ...         userId: 3,
-...         driveDate: new Date("25-oct-2024"),
+...         driveDate: new Date("25-oct-2020"),
 ...         company: "Zoho",
 ...     },
 ...     {
 ...         userId: 2,
-...         driveDate: new Date("27-oct-2024"),
+...         driveDate: new Date("27-oct-2020"),
 ...         company: "TCS",
 ...     },
 ... ]);
